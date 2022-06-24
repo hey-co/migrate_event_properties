5de94e88511c3c2821cb796764eaa446a4be227d
Victor Castaneda <victor.castaneda@hey.net.co>
2022-06-24 17:55:16 +0100

    Add remove left zeros.

--- actions/event/clean_data.py
+++ actions/event/clean_data.py
@@ -1,4 +1,5 @@
 from data_base import main_db
+from typing import List, Tuple, Any
 
 
 class Clean:
@@ -20,10 +21,39 @@ class Clean:
             """
         return query
 
+    def get_property_values(self) -> List[Tuple[Any]]:
+        property_values = self.db_instance.handler(query=f"""
+            SELECT 
+                property_id, value 
+            FROM 
+                event_property 
+            WHERE 
+                event_id in (
+                    select id from user_event where name = '{self.event_name}' LIMIT 200
+                ) 
+            AND 
+                name LIKE '{self.property_name}';
+            """)
+        return property_values
+
+    def update_property_value(self, data):
+        self.db_instance.handler(query=f"""
+            UPDATE
+                event_property            
+            WHERE
+                id = {data[0]}
+            SET
+                value = CAST(({data[1]}) AS int)
+        """)
+
 
 class CleanActions(Clean):
     def __init__(self, event_name, property_name, public_key):
         super().__init__(event_name, property_name, public_key)
+
+    def get_remove_zeros_query(self):
+        for property_value in super().get_property_values():
+            super().update_property_value(data=property_value)
 
     def remove_white_space(self):
         query = super().get_clean_data_query(old_value=" ", new_value="")
