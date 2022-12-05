dfce53995cfeebbda135172ccd31540d583b937a
Victor Castaneda <victor.castaneda@hey.net.co>
2022-12-05 12:13:02 -0500

    Update logic

--- actions/event/mapping.py
+++ actions/event/mapping.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import unidecode
 
 import multitenancy
+load_dotenv()
 
 
 class Mapping:
@@ -23,7 +24,6 @@ class Mapping:
         return multitenancy.get_session_by_secret_key(private_key, dict_db_tenant)
 
     def get_distinct_user_event_names(self):
-
         try:
             distinct_names = self.db.execute("select distinct on (name) name from user_event;")
         except Exception as e:
@@ -33,7 +33,7 @@ class Mapping:
 
     def write_event_schema(self, event_schema_name):
         try:
-            self.db.execute(
+            results = self.db.execute(
                 f"""INSERT INTO event_schema(name, updated_at, created_at, is_active, db_status, is_migrated)
                             VALUES (
                                 '{self.clean_text(text=event_schema_name)}', 
@@ -47,6 +47,30 @@ class Mapping:
             )
         except Exception as e:
             raise e
+        else:
+            self.db.commit()
+            for result in results:
+                self.update_event_property(event_name=event_schema_name, event_schema_id=result[0])
+                return result[0]
+
+    def update_event_property(self, event_name, event_schema_id):
+        event_properties = self.get_event_properties(
+            user_events=self.get_events_by_name(event_name=event_name)
+        )
+        self.move_event_properties(event_properties=event_properties, event_schema_id=event_schema_id)
+
+    def move_event_properties(self, event_properties, event_schema_id):
+        for event_property in event_properties:
+            query = f"""INSERT INTO property_event_schema(
+                        name, type, updated_at, created_at, is_active, event_id)
+                        VALUES ('{event_property[1]}', '{event_property[0]}', '{datetime.now()}', '{datetime.now()}', true, {event_schema_id});"""
+        self.db.execute(query)
+
+    def get_events_by_name(self, event_name):
+        return self.db.execute(f"select * from user_event where name = '{event_name}';")
+
+    def get_event_properties(self, user_events):
+        return self.db.execute(f"select * from event_property where event_name = '{event_name}';")
 
     @staticmethod
     def clean_text(text: str) -> str:
@@ -60,8 +84,8 @@ class Mapping:
 
     def update_unique_schemas(self):
         for distinct_name in self.get_distinct_user_event_names():
-            if not self.validate_column(table_name='event_schema', column_name=distinct_name):
-                self.write_event_schema(event_schema_name=distinct_name)
+            if not self.validate_column(table_name='event_schema', column_name=distinct_name[0]):
+                self.write_event_schema(event_schema_name=distinct_name[0])
 
     def handler(self):
         try:
@@ -90,4 +114,5 @@ class Mapping:
                 table_name=table_name, column_name=self.clean_text(text=column_name)
             )
         )
-        return validate_column
+        for result in validate_column:
+            return result[0]
