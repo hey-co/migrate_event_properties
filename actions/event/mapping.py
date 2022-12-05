c440ebb392d39949bc0e03e71e0b529b60bb3e46
Victor Castaneda <victor.castaneda@hey.net.co>
2022-12-05 10:40:35 -0500

    Update db connection

--- actions/event/mapping.py
+++ actions/event/mapping.py
@@ -1,17 +1,31 @@
 from datetime import datetime
-from data_base import main_db
+import os
+
+from dotenv import load_dotenv
 import unidecode
+
+import multitenancy
 
 
 class Mapping:
-    def __init__(self):
-        self.db = main_db.DBInstance(public_key="***")
+    def __init__(self, event):
+        self.db = self.get_session_db(private_key=event.get("private_key"))
+
+    @staticmethod
+    def get_session_db(private_key: str = None):
+        dict_db_tenant = {
+            "db_name": os.environ.get("MULTI_TENANCY_DB_NAME"),
+            "db_user": os.environ.get("MULTI_TENANCY_DB_USER"),
+            "db_password": os.environ.get("MULTI_TENANCY_DB_PASSWORD"),
+            "db_port": os.environ.get("MULTI_TENANCY_DB_PORT"),
+            "db_host": os.environ.get("MULTI_TENANCY_DB_HOST"),
+        }
+        return multitenancy.get_session_by_secret_key(private_key, dict_db_tenant)
 
     def get_distinct_user_event_names(self):
+
         try:
-            distinct_names = self.db.handler(
-                query="select distinct on (name) name from user_event;"
-            )
+            distinct_names = self.db.execute("select distinct on (name) name from user_event;")
         except Exception as e:
             raise e
         else:
@@ -19,8 +33,8 @@ class Mapping:
 
     def write_event_schema(self, event_schema_name):
         try:
-            self.db.handler(
-                query=f"""INSERT INTO event_schema(name, updated_at, created_at, is_active, db_status, is_migrated)
+            self.db.execute(
+                f"""INSERT INTO event_schema(name, updated_at, created_at, is_active, db_status, is_migrated)
                             VALUES (
                                 '{self.clean_text(text=event_schema_name)}', 
                                 '{datetime.now()}', 
@@ -71,9 +85,9 @@ class Mapping:
         return query
 
     def validate_column(self, table_name: str, column_name: str) -> bool:
-        validate_column = self.db.handler(
-            query=self.get_validate_column_query(
+        validate_column = self.db.execute(
+            self.get_validate_column_query(
                 table_name=table_name, column_name=self.clean_text(text=column_name)
             )
         )
-        return validate_column[0][0]
+        return validate_column
