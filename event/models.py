e2b9ef5974cd23a3d7a6b0e19ff59ac8bf46c7a3
Victor Castaneda <victor.castaneda@hey.net.co>
2022-12-09 09:17:03 -0500

    Add event schema model

---
+++ event/models.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import DateTime, Text, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class EventSchema(Base):
+    __tablename__ = "event_schema"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    help_name = Column(String(254), nullable=True)
+    description = Column(String(2000), nullable=True)
+    status = Column(String, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.now())
+    updated_at = Column(DateTime, nullable=False, default=datetime.now())
+    is_active = Column(Boolean, default=True, nullable=False)
+    is_migrated = Column(Boolean, default=False, nullable=False)
